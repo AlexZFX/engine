e92dd1cfec6c60a6a49af0c5a9e8ebd5833e7780
AlexZFX <z1079911968@163.com>
2018-11-18 10:57:35 +0800

    调整代码结构，准备GG

--- engine_java/src/main/java/com/alibabacloud/polar_race/engine/common/EngineRace.java
+++ engine_java/src/main/java/com/alibabacloud/polar_race/engine/common/EngineRace.java
@@ -6,8 +6,6 @@ import com.carrotsearch.hppc.LongIntHashMap;
 import io.netty.util.concurrent.FastThreadLocal;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import sun.misc.Cleaner;
-import sun.nio.ch.DirectBuffer;
 
 import java.io.File;
 import java.io.IOException;
@@ -21,28 +19,16 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class EngineRace extends AbstractEngine {
 
     private static Logger logger = LoggerFactory.getLogger(EngineRace.class);
-    // key 长度 8B
-//    private static final int KEY_LEN = 8;
-//    // offset 长度 8B
-//    private static final int OFF_LEN = 8;
     // key+offset 长度 16B
     private static final int KEY_AND_OFF_LEN = 12;
     // 线程数量
     private static final int THREAD_NUM = 64;
     // value 长度 4K
     private static final int VALUE_LEN = 4096;
-    //    单个线程写入消息 100w
-//    private static final int MSG_COUNT = 1000000;
-    //    64个线程写消息 6400w
-//    private static final int ALL_MSG_COUNT = 64000000;
     //每个map存储的key数量
     private static final int PER_MAP_COUNT = 1024000;
 
     private static final int SHIFT_NUM = 12;
-
-    //    private static final int ALL_MSG_COUNT = 6400;
-    //    每个文件存放 400w 个数据
-//    private static final int MSG_COUNT_PERFILE = 4000000;
     //    存放 value 的文件数量 128
     private static final int FILE_COUNT = 64;
 
@@ -160,18 +146,14 @@ public class EngineRace extends AbstractEngine {
         int hash = valueFileHash(numkey);
         int off = valueOffsets[hash].getAndIncrement();
         try {
-//            //key写入文件
-//            localKey.get().putLong(0, numkey).putInt(8, off);
-//            localKey.get().position(0);
-//            keyFileChannels[hash].write(localKey.get(), keyOffsets[hash].getAndAdd(KEY_AND_OFF_LEN));
             ByteBuffer buffer = keyMappedByteBuffers[hash].slice();
             buffer.position(keyOffsets[hash].getAndAdd(KEY_AND_OFF_LEN));
             buffer.putLong(numkey).putInt(off);
             //将value写入buffer
-            localBufferValue.get().position(0);
+            localBufferValue.get().clear();
             localBufferValue.get().put(value, 0, VALUE_LEN);
             //buffer写入文件
-            localBufferValue.get().position(0);
+            localBufferValue.get().flip();
             fileChannels[hash].write(localBufferValue.get(), ((long) off) << SHIFT_NUM);
         } catch (IOException e) {
             throw new EngineException(RetCodeEnum.IO_ERROR, "写入数据出错");
@@ -188,12 +170,12 @@ public class EngineRace extends AbstractEngine {
             throw new EngineException(RetCodeEnum.NOT_FOUND, numkey + "不存在");
         }
         try {
-            localBufferValue.get().position(0);
+            localBufferValue.get().clear();
             fileChannels[hash].read(localBufferValue.get(), off << SHIFT_NUM);
         } catch (IOException e) {
             throw new EngineException(RetCodeEnum.IO_ERROR, "读取数据出错");
         }
-        localBufferValue.get().position(0);
+        localBufferValue.get().flip();
         localBufferValue.get().get(localByteValue.get(), 0, VALUE_LEN);
         return localByteValue.get();
     }
@@ -222,11 +204,11 @@ public class EngineRace extends AbstractEngine {
 //        return (int) (key & HASH_KEY);
 //    }
 
-    private void unmap(MappedByteBuffer var0) {
-        Cleaner var1 = ((DirectBuffer) var0).cleaner();
-        if (var1 != null) {
-            var1.clean();
-        }
-    }
+//    private void unmap(MappedByteBuffer var0) {
+//        Cleaner var1 = ((DirectBuffer) var0).cleaner();
+//        if (var1 != null) {
+//            var1.clean();
+//        }
+//    }
 
 }
